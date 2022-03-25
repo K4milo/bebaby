fa71ebe69f34f22a6f76f1c7dc7e0408925257d3
Camilo <kmilo.vanegas@gmail.com>
2022-03-25 15:15:40 -0500

    RC 2.0 - Widget issues fix

--- wp-content/themes/dev_theme/components/follow_us.php
+++ wp-content/themes/dev_theme/components/follow_us.php
@@ -4,8 +4,13 @@
       <a href="https://www.instagram.com/luxuryflowersclt/"
       target="_blank"
       class="follow-us__link">
-      <i class="fab fa-instagram"></i>
-      Follow us on Instagram</a>
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="https://www.facebook.com/luxuryflowersclt/"
+      target="_blank"
+      class="follow-us__link follow-us__link--small">
+        <i class="fab fa-facebook-f"></i>
+      </a>
     </h3>
   </div>
 </section>
--- wp-content/themes/dev_theme/includes/footer.php
+++ wp-content/themes/dev_theme/includes/footer.php
@@ -1,28 +1,23 @@
 <footer class="site-footer">
   <div class="site-sub-footer__wrapper container">
     <div class="row site-footer__top">
-      <div class="col-lg-3 col-md-2 site-footer__widget">
+      <div class="col-lg-4 site-footer__widget">
         <?php dynamic_sidebar('footer-column-1'); ?>
       </div>
-      <div class="col-lg-3 col-md-2 site-footer__widget">
+      <div class="col-lg-4 site-footer__widget">
         <?php dynamic_sidebar('footer-column-2'); ?>
       </div>
-      <div class="col-lg-2 col-md-2 site-footer__widget">
+      <div class="col-lg-4 site-footer__widget">
         <?php dynamic_sidebar('footer-column-3'); ?>
-      </div>
-      <div class="col-lg-4 col-md-2 site-footer__widget">
-        <?php dynamic_sidebar('footer-column-4'); ?>
       </div>
     </div>
     <hr/>
-    <div class="row">
-      <div class="site-footer__bottom">
-        <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
-      </div>
+    <div class="row site-footer__bottom">
+      <div class="col-lg-8"><?php dynamic_sidebar('footer-column-4'); ?></div>
+      <div class="col-lg-4 text-align-right"><p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p></div>
     </div>
   </div>
 </footer>
-
 <?php wp_footer(); ?>
 </body>
 </html>
